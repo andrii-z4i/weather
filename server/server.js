93328fb232121a6d3239eddab3edf2cbf9c03bdb
Andrii Kozin <z4i.andrey@gmail.com>
2015-11-08 14:58:22 +0100

    Issue #1, Introduce search api by city name

--- server/server.js
+++ server/server.js
@@ -1,10 +1,29 @@
 var express = require('express');
 var app = express();
-var bodyParser = require('body-parser')
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 app.use(bodyParser.json()); // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({ // to support URL-encoded bodies
     extended: true
 }));
+
+var connection = mongoose.connection;
+var connected = false;
+connection.on('error', function(err) {console.error(err)});
+connection.once('open', function(){
+    console.log('connected');
+    connected = true;
+});
+
+mongoose.connect('mongodb://localhost/weather');
+
+var CitySchema = new mongoose.Schema({
+    city: [String],  
+    attitude: Number,
+    latitude: Number 
+});
+
+var CityModel = mongoose.model('City', CitySchema);
 
 app.use(function(req, res, next) {
     // Website you wish to allow to connect
@@ -20,20 +39,32 @@ app.use(function(req, res, next) {
     next();
 });
 
-app.get('/city', function(req, res){
+app.get('/city/:city', function(req, res){
     var object = {};
-    if (req.query.search === undefined) {
+    if (connected == false) {
+        object = {error: 'can\'t connect to database'};
+    }
+    else if (req.params.city === undefined) {
         object = {error: 'search is undefined'};
 
     } else {
-        var _cityToSearch = req.query.search;
+        var _cityToSearch = req.params.city;
         console.log(_cityToSearch);
-        object = {city: _cityToSearch, attitude: '1', latitude: '2'};
+        CityModel.find({city: {$elemMatch: {$eq: _cityToSearch}}}, function(err, result){
+            if (err) {
+                res.end(JSON.stringify({error: err}));
+            }
+            else {
+                console.log(result);
+                res.end(JSON.stringify({results: result}));
+            }
+        });
+        return;
     }
     res.end(JSON.stringify(object));
 });
 
-app.get('forecast', function(req, res){
+app.get('forecast/:attitude/:latitude', function(req, res){
 
     res.end({temperature: 22});
 });
